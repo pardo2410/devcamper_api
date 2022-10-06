3c3f31749992600d9d7cd6e00634a7d6555cc747
Juan David Pardo Pinzón <pardo2410@gmail.com>
2022-10-06 17:50:24 +0200

    create register & login

---
+++ controllers/auth.js
@@ -0,0 +1,53 @@
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const User = require("../models/User");
+
+// @desc register user
+// @route POST /api/v1/auth/register
+// @access Public
+exports.register = asyncHandler(async (req, res, next) => {
+    const {name, email, password, role } = req.body;
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role
+    });
+
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({success: true, token});
+});
+
+// @desc login user
+// @route POST /api/v1/auth/login
+// @access Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const {email, password } = req.body;
+
+    // Validate email & password
+    if(!email || !password){
+        return next(new ErrorResponse("Please provide an enamil and password", 400));
+    };
+
+    // Check for user
+    const user = await User.findOne({email}).select("+password");
+
+    if(!user){
+        return next(new ErrorResponse("Invalid credentials", 401));
+    };
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+    if(!isMatch){
+        return next(new ErrorResponse("Invalid credentials", 402))
+    };
+
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({success: true, token});
+});
--- server.js
+++ server.js
@@ -19,6 +19,7 @@ connectDB();
 // Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 const app = express();
 
@@ -41,6 +42,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 app.use(errorHandler);
 
